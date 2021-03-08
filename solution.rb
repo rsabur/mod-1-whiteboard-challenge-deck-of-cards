4635c351a08bda6a3a28d48a04fea936abe01fc9
Radiyah <rsabur58@gmail.com>
2021-03-08 09:30:35 -0500

    commit tree 9aaa0b5e838170ee577847afb2e7c7de8c07a9d5

--- solution.rb
+++ solution.rb
@@ -1,7 +1,23 @@
 class Deck
 
+    attr_reader :cards
+    @deck = []
+
+    def initialize
+        suits = ["Hearts", "Clubs", "Diamonds", "Spades"]
+        ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
+    end
+
+    def choose_card
+    end
 end
 
 class Card
+    attr_reader :rank, :suit
 
+    def initialize(suit, rank)
+        @rank = rank
+        @suit = suit
+    end
 end
